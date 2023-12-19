3710a0447d12ae9fa47038edd56718018e6bde7b
a13v3r <verbitskiy_al@mail.ru>
2023-12-19 17:33:44 +0300

    hiding and updating secrets

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,16 +10,12 @@ from storage.db import create_pool, get_session_maker, proceed_schemas, BaseMode
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-
-    print("asonas[dasd")
     dp = Dispatcher()
     bot = Bot(token=os.getenv('bot_token'))
 
     register_user_commands(dp)
 
-    url = "mysql+aiomysql://root:2Ghgb6fhBDBA5DaA2hhh-gBe233B-dA-@viaduct.proxy.rlwy.net:15123/railway"
-
-    async_engine = create_pool(url)
+    async_engine = create_pool(os.getenv('db_path'))
     session_maker = get_session_maker(async_engine)
     await proceed_schemas(async_engine, BaseModel.metadata)
 

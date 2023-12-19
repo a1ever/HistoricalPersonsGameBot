5266e82c93002c6cd6d1d2996875243f8a2da581
a13v3r <verbitskiy_al@mail.ru>
2023-12-19 18:11:12 +0300

    no logging here

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,7 +9,6 @@ from storage.db import create_pool, get_session_maker, proceed_schemas, BaseMode
 
 
 async def main():
-    logging.basicConfig(level=logging.INFO)
     dp = Dispatcher()
     bot = Bot(token=os.getenv('bot_token'))
 
